76d7656c4a628a55994cd6b0a5797863062b4e25
gaoge <youyaochihaha@qq.com>
2016-10-05 01:04:17 +0800

    update default static folder

--- retoohs/settings_local_sample.py
+++ retoohs/settings_local_sample.py
@@ -4,4 +4,4 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 DEBUG = True
 DB_TYPE = 'sqlite'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_out')
+STATIC_ROOT = '/var/www/retoohs/static'
