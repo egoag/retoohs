e94d2d5b647b796e3c07c7daf5476ccb3180c5ef
youyaochi <youyaochi@gmail.com>
2016-10-07 17:23:26 +0800

    add donation

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,11 @@
-from django.conf.urls import url, include
+from django.conf.urls import url
+from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
     url(r'^register$', views.Register.as_view(), name='register'),
     url(r'^login$', views.login, name='login'),
     url(r'^logout$', views.logout, name='logout'),
+    url(r'^donate$', TemplateView.as_view(template_name='main/donate.html'), name='donate'),
     url(r'^my$', views.UserInfo.as_view(), name='my'),
 ]
